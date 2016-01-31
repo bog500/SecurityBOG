4bb400a4af9ff1fb787a29d2dfb6faecb175f7bc
limbo_9@hotmail.com <limbo_9@hotmail.com>
2016-01-31 00:21:45 -0500

    command

--- mc.securitybog/src/mc/securitybog/SecurityBOG.java
+++ mc.securitybog/src/mc/securitybog/SecurityBOG.java
@@ -1,6 +1,10 @@
 package mc.securitybog;
 
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -8,9 +12,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -31,18 +37,22 @@ public class SecurityBOG extends JavaPlugin implements CommandExecutor, Listener
 	private PluginDescriptionFile mPdfFile;
 	protected static ConfigAccessor language;
 	private JsonSecurityBOG security;
+	private HashSet<Location> spawnLocations = new HashSet<Location>();
+	private Boolean randomSpawn = false;
+	private Random rand = new Random();
+	private List<Map<?, ?>> spawnLocationsList;
 	
 	@Override
 	public void onEnable() {
 		mPdfFile = this.getDescription();
 				
+		getSavedConfig();
+		
 		new JUtility(this);
 		
 		callMetric();
 
 		checkUpdates();
-		
-		
 		
 		this.disabled = false;
 		
@@ -60,6 +70,33 @@ public class SecurityBOG extends JavaPlugin implements CommandExecutor, Listener
 		security = new JsonSecurityBOG(this);
 	}
 	
+	private void getSavedConfig() {
+		FileConfiguration config = getConfig();
+		this.randomSpawn = config.getBoolean("randomFirstSpawn");
+		
+		spawnLocationsList = config.getMapList("spawnLocations");
+
+		for (Map.Entry<?, ?> entry : spawnLocationsList.get(0).entrySet()) {
+			String key = entry.getKey().toString().toLowerCase();
+			if(Boolean.parseBoolean(entry.getValue().toString())) {
+				String[] values = key.split(" ");
+				World world = Bukkit.getServer().getWorld(values[0]);
+				double x = Double.parseDouble(values[1]);
+				double y = Double.parseDouble(values[2]);
+				double z = Double.parseDouble(values[3]);
+				Location l = new Location(world, x, y, z);
+				this.getLogger().info(world.getName() + " " + x + " " + y + " " + z);
+				if (Boolean.parseBoolean(entry.getValue().toString())) {
+					this.getLogger().info("true");
+					if (!spawnLocations.contains(l)) {
+						this.getLogger().info("added");
+						spawnLocations.add(l);
+					}
+				}
+			}
+		}
+	}
+
 	private void callMetric() {
 
 		try {
@@ -103,10 +140,17 @@ public class SecurityBOG extends JavaPlugin implements CommandExecutor, Listener
     }
 	
 	private Location setSpawn(Player player) {
-		JUtility.sendMessage(player, language.getConfig().getString("teleporting_player").replace("{player}", player.getName()) );
-		Location spawnLoc = new Location(Bukkit.getServer().getWorld("world"), 0, 0, 0);
-		player.teleport(spawnLoc);
-		return spawnLoc;
+		if(randomSpawn) {
+			JUtility.sendMessage(player, language.getConfig().getString("teleporting_player").replace("{player}", player.getName()) );
+			
+			Integer index = rand.nextInt(this.spawnLocations.size());
+			Location[] l = new Location[this.spawnLocations.size()];
+			Location[] spawnLocs = spawnLocations.toArray(l);
+			Location spawnLoc = spawnLocs[index];
+			player.teleport(spawnLoc);
+			return spawnLoc;
+		}
+		return null;
 	}
 	
 	@EventHandler(priority = EventPriority.MONITOR)
@@ -114,7 +158,7 @@ public class SecurityBOG extends JavaPlugin implements CommandExecutor, Listener
 		Player player = event.getPlayer();
 		if(!event.isBedSpawn() || player.getBedSpawnLocation() == null)
 		{
-			if (!player.hasPlayedBefore() || player.getBedSpawnLocation() == null ) {
+			if (randomSpawn && (!player.hasPlayedBefore() || player.getBedSpawnLocation() == null )) {
 				Location spawnLoc = setSpawn(player);
 				event.setRespawnLocation(spawnLoc);
 			}
@@ -148,6 +192,34 @@ public class SecurityBOG extends JavaPlugin implements CommandExecutor, Listener
 
 	@Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("spawntp")) {
+			if(args.length == 1) {
+				return spawntp(sender, args);
+			}
+		}
+		return false;
+	}
+
+	private boolean spawntp(CommandSender sender, String[] args) {
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			if (!player.hasPermission("spawntp")) {
+				JUtility.sendMessage(player, language.getConfig().getString("no_permission") );
+				return true;
+			}
+			
+			if(args.length != 1) {
+				return false;
+			}
+			int index = Integer.parseInt(args[0]);
+			index--; // convert it to start at 0
+			Location[] l = new Location[this.spawnLocations.size()];
+			Location[] spawnLocs = spawnLocations.toArray(l);
+			Location spawnLoc = spawnLocs[index];
+			JUtility.sendMessage(player, language.getConfig().getString("teleporting_player").replace("{player}", player.getName()) );
+			player.teleport(spawnLoc);
+		}
+		sender.sendMessage("This command can not be used in console");
 		return true;
 	}
 	
